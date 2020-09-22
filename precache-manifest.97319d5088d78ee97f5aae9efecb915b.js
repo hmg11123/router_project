7039497afb194bb4a546e9a7bc9fac37ed176bfd
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-09-22 23:44:44 +0900

    Updates

---
+++ precache-manifest.97319d5088d78ee97f5aae9efecb915b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd7345c8153efc91e22a7f671e25ef3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a000cbf3d35113e3c02e",
+    "url": "/static/css/main.3c1c9a4b.chunk.css"
+  },
+  {
+    "revision": "e966e66b27bce03d285e",
+    "url": "/static/js/2.1ed18bd1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1ed18bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a000cbf3d35113e3c02e",
+    "url": "/static/js/main.50727f34.chunk.js"
+  },
+  {
+    "revision": "33973b3c7e897e0d907a",
+    "url": "/static/js/runtime-main.e7553a76.js"
+  },
+  {
+    "revision": "300ea511c047519e9e28ec302ec6310e",
+    "url": "/static/media/coffee.300ea511.jpg"
+  },
+  {
+    "revision": "2640609feac0ec4b16808d61072096c7",
+    "url": "/static/media/coffee02.2640609f.jpg"
+  }
+]);
